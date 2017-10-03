6d1fa7369b1e0964e4d90b3ed709e5e4b6723650
Denis <denisglb@yahoo.co.uk>
2017-10-03 16:06:58 +0100

    rubocop updated

--- spec/account_spec.rb
+++ spec/account_spec.rb
@@ -1,6 +1,11 @@
 require_relative '../lib/account'
 
 describe Account do
+  DEPOSIT = [100, '2011-01-12'].freeze
+  WITHDRAW = [50, '2011-01-12'].freeze
+  trans = [{ date: '2011-01-12', credit: 100, balance: 100 }]
+  subject.deposit(DEPOSIT)
+
   it 'responds to deposit money' do
     expect(subject).to respond_to :deposit
   end
@@ -13,21 +18,18 @@ describe Account do
   end
 
   it 'adds value to balance' do
-    expect { subject.deposit(100, '2011-01-12') }.to change { subject.balance }.by(100)
+    expect { subject.deposit(DEPOSIT) }.to change { subject.balance }.by(100)
   end
 
   it 'removes value from balance' do
-    subject.deposit(100, '2011-01-12')
-    expect { subject.withdraw(50, '2011-01-12') }.to change { subject.balance }.by(-50)
+    expect { subject.withdraw(WITHDRAW) }.to change { subject.balance }.by(-50)
   end
 
   it 'shows the balance' do
-    subject.deposit(100, '2011-01-12')
     expect(subject.balance).to eq(100)
   end
 
   it 'saves the date of the transaction' do
-    subject.deposit(100, '2011-01-12')
-    expect(subject.account).to eq([{ date: '2011-01-12', credit: 100, balance: 100 }])
+    expect(subject.account).to eq(trans)
   end
 end
--- spec/bank_spec.rb
+++ spec/bank_spec.rb
@@ -3,6 +3,8 @@ require_relative '../lib/bank'
 describe Bank do
   let(:account) { double(:account) }
   arr = [{ date: '2011-01-12', credit: 100, balance: 100 }]
+  show = "date || credit || debit || balance\n"
+  state = "2011-01-12 || 100 ||  || 100\n"
 
   it 'responds to display' do
     expect(subject).to respond_to :display
@@ -13,12 +15,12 @@ describe Bank do
   end
 
   it 'displays the header of the bank account statemetn' do
-    expect { subject.display }.to output("date || credit || debit || balance\n").to_stdout
+    expect { subject.display }.to output(show).to_stdout
   end
 
   it 'shows a statement of accounts' do
     allow(account).to receive(:account).and_return(arr)
     subject = Bank.new(account)
-    expect { subject.statement }.to output("2011-01-12 || 100 ||  || 100\n").to_stdout
+    expect { subject.statement }.to output(state).to_stdout
   end
 end
